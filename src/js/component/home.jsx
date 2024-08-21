2ca11764e274db9b5c624024de180cf8c6c340b6
Matias Rivas <mat500.600@gmail.com>
2024-08-21 15:31:30 -0400

    creando esqueleto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,18 @@
 import React from "react";
+import style from "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container">
+      <ul>
+        <li>task 1</li>
+        <li>task 2</li>
+        <li>task 3</li>
+        <li>task 4</li>
+        <li>task 5</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
