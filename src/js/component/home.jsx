33fed35c078648dc5a807abf06fbf662015456dd
Matias Rivas <mat500.600@gmail.com>
2024-08-22 16:06:27 -0400

    agregada funcionalidad, falta parte visual

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "../../styles/index.css";
 
 const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleNewTask = (e) => {
+    if (e.key === "Enter" && inputValue.trim()) {
+      setToDos([...toDos, inputValue]);
+      setInputValue("");
+    }
+  };
+  const HandleRemoveTask = (index) => {
+    const newToDos = toDos.filter((_, i) => i !== index);
+    setToDos(newToDos);
+  };
   return (
     <div className="container">
+      <h1>To-Dos!</h1>
+      <h2>Add or remove tasks</h2>
       <ul>
-        <li>task 1</li>
-        <li>task 2</li>
-        <li>task 3</li>
-        <li>task 4</li>
-        <li>task 5</li>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInput}
+          onKeyDown={handleNewTask}
+          placeholder="Type your task here!"
+        />
+        {toDos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button className="delete" onClick={() => HandleRemoveTask(index)}>
+              x
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
